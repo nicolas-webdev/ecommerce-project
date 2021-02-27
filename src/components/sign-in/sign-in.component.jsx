941161a4d8e28578801bc7e17a06dde71155fecf
Nicolas <nicobsas+webdev@gmail.com>
2021-02-27 20:05:07 +0900

    登録セクションの追加

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+import FormInput from "../form-input/form-input.component";
+
+import "./sign-in.styles.scss";
+
+const SignIn = () => {
+  //State
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  //ハンドル
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setFormData({ email: "", password: "" });
+  };
+  const handleChange = (e) => {
+    const { value, name } = e.target;
+    setFormData({ ...formData, [name]: value });
+  };
+
+  return (
+    <div className="sign-in">
+      <h2>アカウントをお持ちの方</h2>
+      <span>メール・パスワードでログイン</span>
+
+      <form onSubmit={handleSubmit}>
+        <FormInput
+          required
+          name="email"
+          type="email"
+          label="メールアドレス"
+          value={formData.email}
+          handleChange={handleChange}
+        />
+
+        <FormInput
+          required
+          name="password"
+          type="password"
+          label="パスワード"
+          value={formData.password}
+          handleChange={handleChange}
+        />
+
+        <input type="submit" value="登録" />
+      </form>
+    </div>
+  );
+};
+
+export default SignIn;
