3bc491b2751d8e68ad2f48215d0e4ffdce7e83d3
Nicolas <nicobsas+webdev@gmail.com>
2021-03-01 00:11:15 +0900

    環境についてコメント

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,9 +1,17 @@
+//Redux Store
+
 import { createStore, applyMiddleware } from "redux";
 import { persistStore } from "redux-persist";
 import rootReducer from "./root-reducer";
 import logger from "redux-logger";
 
-//Reduxで何がどう変わってるのかを御覧いただけるように
+//通常は開発中にしかLoggerを使いたくない
+// if (process.env.NODE_ENV === 'development') {
+//   middlewares.push(logger);
+// }
+
+//ですが、
+//Reduxで何がどう変わってるのかを御覧いただけるように残します
 const middlewares = [logger];
 
 //ストアの状態をローカルストレージで保存
