a33ac1d3be44d3f36ddcdb70f68b82747ceb8466
Nicolas <nicobsas+webdev@gmail.com>
2021-02-28 17:47:50 +0900

    支払いページから、アイテムの追加と排除機能

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -16,3 +16,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
   //入ってなければ、Quantityは１
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+//上記と逆、多数が入っていれば−１、個数が１なら削除
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+  const existingCartItem = cartItems.find(
+    (cartItem) => cartItem.name === cartItemToRemove.name
+  );
+  if (existingCartItem.quantity === 1) {
+    return cartItems.filter(
+      (cartItem) => cartItem.name !== cartItemToRemove.name
+    );
+  }
+  return cartItems.map((cartItem) =>
+    cartItem.name === cartItemToRemove.name
+      ? { ...cartItem, quantity: cartItem.quantity - 1 }
+      : cartItem
+  );
+};
